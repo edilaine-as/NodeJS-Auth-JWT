c1507f299408f37c1389195b0dd531addc3b6ab0
Edilaine <edilaine.santos839@gmail.com>
2025-02-18 21:04:15 -0300

    feat: add private route (token-based authentication)

--- app.js
+++ app.js
@@ -17,6 +17,41 @@ app.get('/', (req, res) => {
     res.status(200).json({msg: 'Bem vindo a nossa API!'})
 })
 
+// Private Route
+app.get("/user/:id", checkToken, async (req, res) => {
+    const id = req.params.id;
+    
+    // check if user exists
+    const user = await User.findById(id, "-password");
+
+    if (!user) {
+        return res.status(404).json({ msg: "Usuário não encontrado!" });
+    }
+
+    res.status(200).json({ user });
+});
+
+
+function checkToken(req, res, next){
+    const authHeader = req.headers.authorization
+    const token = authHeader?.split(" ")[1]
+
+    if(!token){
+        return res.status(401).json({ msg: 'Acesso negado!' })
+    }
+
+    try{
+        const secret = process.env.SECRET
+
+        jwt.verify(token, secret)
+
+        next()
+        
+    } catch(error){
+        res.status(400).json({ msg: "Token inválido!" })
+    }
+}
+
 // Register User
 app.post('/auth/register', async (req, res) => {
     const { name, email, password, confirmpassword } = req.body
@@ -65,7 +100,7 @@ app.post('/auth/register', async (req, res) => {
 
         res.status(500).json({msg: 'Aconteceu um erro no servidor, tente novamente mais tarde!'})
     }
-})
+}) 
 
 // Login User
 app.post('/auth/login', async (req, res) => {
